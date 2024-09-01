ef2191301c38fa6bc249cd79440734175bfe7c14
eugene0318 <nttott@gmail.com>
2024-09-01 19:49:05 +0900

    ~ end

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentResolver.Login;
 import hello.login.web.session.SessionManager;
 
 @Slf4j
@@ -61,8 +62,9 @@ public class HomeController {
 		return "loginHome";
 	}
 
-	@GetMapping("/")
-	public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+//	@GetMapping("/")
+	public String homeLoginV3Spring(
+			@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
 
 //		HttpSession session = request.getSession(false);
 //		if (session != null) {
@@ -70,6 +72,18 @@ public class HomeController {
 //		}
 //		;
 //		Object loginMember = session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+		if (loginMember == null) {
+			return "home";
+		}
+
+		model.addAttribute("member", loginMember);
+		return "loginHome";
+	}
+
+	@GetMapping("/")
+	public String homeLoginV3ArgumentResolver(
+			@Login Member loginMember, Model model) {
 
 		if (loginMember == null) {
 			return "home";
